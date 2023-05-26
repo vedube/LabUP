9a3e93d50afd8d3b1ca34d46fc9d9d6cfd5e7422
Valerie Dube <valerie@MacBook-Air.local>
2023-05-25 22:34:33 -0500

    Modificar página de decisión y resultados

---
+++ public_goods_simple/__init__.py
@@ -0,0 +1,53 @@
+from otree.api import *
+
+
+class C(BaseConstants):
+    NAME_IN_URL = 'public_goods_simple'
+    PLAYERS_PER_GROUP = 4 # 4 jugadores
+    NUM_ROUNDS = 1
+    ENDOWMENT = 8 # 8 soles a c/u
+    MULTIPLIER = 2 # las ganancias se multiplican x2
+
+
+class Subsession(BaseSubsession):
+    pass
+
+
+class Group(BaseGroup):
+    total_contribution = models.CurrencyField()
+    individual_share = models.CurrencyField()
+
+
+class Player(BasePlayer):
+    contribution = models.CurrencyField(
+        min=0, max=C.ENDOWMENT, label="¿Cuánto contribuirás al proyecto grupal?" # pregunta del botón
+    )
+
+
+# FUNCTIONS
+def set_payoffs(group: Group):
+    players = group.get_players()
+    contributions = [p.contribution for p in players]
+    group.total_contribution = sum(contributions)
+    group.individual_share = (
+        group.total_contribution * C.MULTIPLIER / C.PLAYERS_PER_GROUP
+    )
+    for p in players:
+        p.payoff = C.ENDOWMENT - p.contribution + group.individual_share
+
+
+# PAGES
+class Contribute(Page):
+    form_model = 'player'
+    form_fields = ['contribution']
+
+
+class ResultsWaitPage(WaitPage):
+    after_all_players_arrive = set_payoffs
+
+
+class Results(Page):
+    pass
+
+
+page_sequence = [Contribute, ResultsWaitPage, Results]
